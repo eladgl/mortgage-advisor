d587d00838842f86f75f56e72795a23523904e42
eladgl <59554824+eladgl@users.noreply.github.com>
2024-03-08 20:21:32 +0200

    add range component

---
+++ mortgage-advisor-client/src/components/rangeSlider.jsx
@@ -0,0 +1,84 @@
+import React, { useEffect, useRef, useState } from 'react';
+import styled from 'styled-components';
+import { Label } from "../components/label";
+import { Input } from "../components/input";
+
+const RangeWrapper = styled.div`
+    position: relative;
+    display: flex;
+    flex-direction: row-reverse;
+`;
+
+const RangeInput = styled(Input)`
+  direction: ltr;
+`;
+
+const Hint = styled.div`
+  position: absolute; 
+  top: 2rem;
+  left: ${(props) => `${props.left}%`};
+  color: white; 
+  border: 1px solid blue; 
+  border-radius: 50%;
+  background-color: #6d6dfe;
+
+  width: max-content;
+  padding: 6px;
+  justify-content: center;
+    border-radius: 50%;
+    border-top-right-radius: 0;
+    transform: rotate(-45deg);
+`;
+
+const TextInHint = styled.span`
+    display: inline-block;
+    transform: rotate(45deg);
+    font-size: 4px;
+`;
+
+const RangeSlider = ({ minValue, maxValue }) => {
+    const [value, setValue] = useState(0);
+    const [hintPosition, setHintPosition] = useState(0);
+    const rangeRef = useRef(null);
+    const hintRef = useRef(null);
+
+    const handleChange = (event) => {
+        const val = parseInt(event.target.value);
+        setValue(val);
+        updatePosition(val);
+    };
+
+    const updatePosition = (val) => {
+        if (rangeRef.current && hintRef.current) {
+            const clientWidth = rangeRef.current.clientWidth;
+            const rangeVal = rangeRef.current.value;
+            const offset = (rangeVal - minValue) * (100 / (maxValue - minValue));
+            const hintWidth = hintRef.current.offsetWidth;
+            const hintOffset = offset - (hintWidth / clientWidth) * offset;
+            setHintPosition(hintOffset);
+        }
+    };
+
+    useEffect(() => {
+        updatePosition();
+    }, [hintPosition]);
+
+    return (
+        <RangeWrapper className="range-slider">
+            <Label>{minValue}</Label>
+            <RangeInput
+                type="range"
+                min={minValue}
+                max={maxValue}
+                ref={rangeRef}
+                value={value}
+                onChange={handleChange} />
+            <Hint ref={hintRef} left ={hintPosition}>
+                <TextInHint>{`${value}₪`}</TextInHint>
+            </Hint>
+            <Label>{maxValue}</Label>
+        </RangeWrapper>
+    );
+};
+
+export default RangeSlider;
--- mortgage-advisor-client/src/pages/normalMortgage.jsx
+++ mortgage-advisor-client/src/pages/normalMortgage.jsx
@@ -1,7 +1,71 @@
 import React from "react";
 
+import styled from 'styled-components';
+
+import Select from "../components/select";
+import { ImportantLabel, Label } from "../components/label";
+import { Input } from "../components/input";
+import RangeSlider from "../components/rangeSlider";
+
+const Wrapper = styled.div`
+  padding: 1.5rem;
+  margin-bottom: 1rem;
+  border: 1px solid black;
+  background-color: white;
+  border-radius: 1rem;
+
+  @media (min-width: 640px) {
+    padding: 2rem;
+    margin-top: 2rem;
+    margin-bottom: 2rem;
+  }
+`;
+
+const citizenships = ['ישראלית', 'אמריקאית', 'אירופאית'];
+const payBackMethod = ['ריבית פריים', 'ריבית קבועה צמודה למדד', 'ריבית קבועה לא צמודה'];
+
+const GridCell = styled.div`
+    
+`;
+
 const NormalMortgage = () => {
-    return <p>NormalMortgage</p>
+    
+    const renderGrid = () => {
+
+        return (
+            <div className="grid grid-cols-2 grid-rows-2 gap-4">
+                <GridCell>
+                    <ImportantLabel>שם בנק שבו מתהל חשבונך</ImportantLabel>
+                    <Input
+                        type="text"
+                        name="bankNaame"
+                        id="bankName"
+                        placeholder="לחץ כדי לבחור"
+                        required/>
+                </GridCell>
+                <GridCell>
+                    <ImportantLabel>כמה כסף אתה מעוניין להלוות? [בש"ח]</ImportantLabel>
+                    <RangeSlider minValue={ 0 } maxValue={ 100 }/>
+                </GridCell>
+                <GridCell>
+                    <ImportantLabel>בחר שיטת החזר רצויה</ImportantLabel>
+                    <Select id="citizenship" name="citizenship" options={payBackMethod} />
+                </GridCell>
+                <GridCell>
+                    <Label>אזרחות נוכחית</Label>
+                    <Select id="citizenship" name="citizenship" options={citizenships} />
+                </GridCell>
+            </div>
+        );
+    };
+
+    return (
+        <Wrapper>
+            {renderGrid()}
+        </Wrapper>
+    );
+
+    //return <Select options={["ישראלית", "אמריקאית"]} />
 };
 
 export default NormalMortgage;
