886ac51a5b3c8b0465aa29da5ba082d5e752a56e
Elad Goldenberg <59554824+eladgl@users.noreply.github.com>
2024-03-10 14:28:22 +0200

    few loans (#12)

--- mortgage-advisor-client/src/pages/fewLoans.jsx
+++ mortgage-advisor-client/src/pages/fewLoans.jsx
@@ -1,7 +1,66 @@
 import React from "react";
 
+import styled from 'styled-components';
+import { LeftArrow } from "../components/arrows";
+import { TailWindLink } from "../components/link";
+
+const Wrapper = styled.div`
+  padding: 1.5rem;
+  height: 100%;
+  margin-bottom: 1rem;
+  border: 1px solid black;
+  background-color: white;
+  border-radius: 1rem;
+
+  @media (min-width: 640px) {
+    padding: 2rem;
+    margin-top: 2rem;
+    margin-bottom: 2rem;
+  }
+`;
+
+const ChosenTitles = styled.h1`
+    font-size: 25px;
+    font-weight: 700;
+    color: green;
+`;
+
+const AlertLabel = styled.h1`
+    font-size: 16px;
+    font-weight: 700;
+    color: red;
+    margin-top: 3rem;
+`;
+
+const TitleBorderDiv = styled.div`
+    border-bottom: 5px solid grey;
+
+`
+
+
 const FewLoans = () => {
-    return <p>New fewLoans</p>
+    return (
+        <Wrapper>
+            <TitleBorderDiv>
+            <h3>מעוניין לקחת הלוואה רגילה</h3>
+            <h3>מעוניין לקחת הלוואה עד 100 אלף ש"ח</h3>
+            <ChosenTitles>אני מעוניין לקחת כמה הלוואות</ChosenTitles>
+            </TitleBorderDiv>
+            <div>
+                <AlertLabel>אין לך אפשרות לקחת כמה הלוואות עבור סכום הקטן מ100 אלף ש"ח</AlertLabel>
+                <p style={{fontWeight: '700', fontSize:'1.6rem'}} >אופציות אחרות</p>
+                <div className="grid grid-cols-3 grid-rows-1 gap-4" style={{ margin: '3rem 0 -3rem 0'}}>
+                    <p>הגשת בקשה <br />פרטנית בבנק</p>
+                    <p>הבנק בודק את<br />הבקשה</p>
+                    <p>החלטה של הבנק<br />האם לאשר<br />הלוואה פרטנית</p>
+                </div>
+                <LeftArrow />
+                <p>מחפש עזרה בתהליך? <TailWindLink href="contactUs">פנה אלינו לפרטים</TailWindLink></p>
+                <p style={{fontWeight: '700', fontSize:'1.6rem'}} >עלויות משוערות</p>
+                <p>פתיחת בקשה פרטנית: 500 ש"ח</p>
+            </div>
+        </Wrapper>
+    )
 };
 
 export default FewLoans;
