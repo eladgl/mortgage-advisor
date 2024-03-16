5390f418d758405cb00e080f1cf4b2b1b425bcac
[SharonMor] <[sharonmor5869@gmail.com]>
2024-03-16 13:06:45 +0200

    fix-homepage-mobile

--- mortgage-advisor-client/src/pages/homepage.jsx
+++ mortgage-advisor-client/src/pages/homepage.jsx
@@ -9,16 +9,13 @@ import styled from 'styled-components';
 import Carousel from "../components/carousel";
 
 const Heading = styled.h1`
-    padding-left: 3rem;
-    font-size: 3.5rem;
-    text-transform: uppercase;
     font-weight: 700;
     display: inline-block;
     background-image: linear-gradient(to right, #7ed56f, #28b485);
     -webkit-background-clip: text;
     background-clip: text;
     color: transparent;
-    letter-spacing: .2rem;
+    letter-spacing: .1rem;
     transition: all .2s;
     text-align: center;
     justify-content: center;
@@ -34,9 +31,9 @@ const HomePage = () => {
     return (
         <div>
             <h1 className="text-4xl text-center text-black font-bold pt-20">ברוכים הבאים לאתר שלנו</h1>
-            <div className='flex flex-co-2 mt-8 justify-center'>
-                <Heading className="text-4xl">שרון מור - 207470329</Heading>
-                <Heading className="text-4xl">אלעד גולדנברג - 315040519</Heading>
+            <div className='flex flex-co-2 mt-8 justify-center gap-5 '>
+                <Heading className="text-xl lg:text-4xl md:text-2xl">שרון מור - 207470329</Heading>
+                <Heading className="text-xl md:text-2xl lg:text-4xl">אלעד גולדנברג - 315040519</Heading>
             </div>
             
             <Carousel images={[firstImage, secondImage, thirdImage]} />
